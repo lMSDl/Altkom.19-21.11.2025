ee5bf99ce36b08530229cb5bbf8f8e506aba8621
Paweł <pawel@msd.info.pl>
2025-11-20 11:31:10 +0100

    Środowiska

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
+//miejsce na konfiguracjê DI, Loggera, Konfiguracji
 
-
+//wbudowana konfiguracja
 //builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+//³adowanie opcjonalnego pliku konfiguracyjnego zale¿nego od œrodowiska
+//builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 //dostep do konfiguracji
 //builder.Configuration
@@ -14,6 +17,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
+//miejsce na konfiguracjê œcie¿ki przetwarzania ¿¹dañ
+
+//domyœlnie, jeœli jest œrodowisko deweloperskie, to w³¹cza stronê b³êdów deweloperskich na której s¹ szczegó³y wyj¹tku wraz z stack trace
+//w przeciwnym wypadku jest strona b³êdu 500 (Internal Serrver Error)
+/*if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}*/
 
 //dostep do konfiguracji
 //app.Configuration
@@ -21,6 +32,29 @@ var app = builder.Build();
 //dostep do DI - IServiceProvider
 //app.Services
 
-app.MapGet("/", () => "Hello World!");
+
+//sposoby sprawdzania œrodowiska
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/", () => "Hello from development!");
+}
+else if(app.Environment.IsStaging())
+{
+    app.MapGet("/", () => "Hello from staging!");
+}
+else if(app.Environment.IsProduction())
+{
+    app.MapGet("/", () => "Hello from production!");
+}
+else if (app.Environment.IsEnvironment("Alamakota"))
+{
+    app.MapGet("/", () => "Hello from alamakota!");
+}
+
+//test developer exception page
+app.MapGet("/error", x => throw new Exception());
+
+//wstrzykiwanie loggera do metody anonimowej
+//app.MapGet("/", (ILogger<Program> logger) => { logger.LogDebug("ala ma kota"); return "Hello World!"; });
 
 app.Run();
